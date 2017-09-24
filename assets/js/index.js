8e0250dd2ac603c508b7848df2f01c549e5bc529
Omar Habash <ohabash@successpartners.com>
2017-09-24 16:56:42 -0500

    added-webpack-bundljs

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+// import './bower_components/jquery/dist/jquery.min.js';
+var $ = require('./../bower/jquery/dist/jquery.min.js');
+window.jQuery = $;
+window.$ = $;
+import './ng-app.js';
+import './notice.js';
+import './main.js';
+
